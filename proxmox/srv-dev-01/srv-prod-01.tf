a89ed88c846b3d465eb82e9ec2c5c60358c74d12
Ryan H <rhamamur@gmail.com>
2023-08-04 06:51:48 +0000

    Reorganize file structure

--- proxmox/srv-dev-01/srv-prod-01.tf
+++ proxmox/srv-dev-01/srv-prod-01.tf
@@ -1,34 +1,34 @@
-resource "proxmox_vm_qemu" "srv_test_1" {
-  name = "srv-test-1"
+resource "proxmox_vm_qemu" "srv_dev_01" {
+  name = "srv-dev-01"
   desc = "Ubuntu Server"
-  vmid = "401"
+  vmid = "402"
   target_node = "proxmox"
 
   agent = 1 
 
   clone = "ubuntu-server-focal-docker"
   full_clone = true 
-  cores = 4
+  cores = 2
   sockets = 1 
   cpu = "host"
-  memory = 4096
+  memory = 2048
   
   network {
-    bridge = "vmbr0"
+    bridge = "vmbr2"
     model = "virtio"
   }
 
   disk {
     storage = "local-lvm"
     type = "virtio"
-    size = "32G"
+    size = "64G"
     format = "raw"
   }
   
   scsihw = "virtio-scsi-pci"
   os_type = "cloud-init"
   cloudinit_cdrom_storage = "local-lvm"
-  ipconfig0 = "ip=192.168.55.3/16,gw=192.168.55.1"
+  ipconfig0 = "ip=192.168.55.4/24,gw=192.168.55.1"
   nameserver = "192.168.55.1"
   ciuser = "ryan"
   sshkeys = <<EOF
---
+++ proxmox/srv-prod-01/srv-prod-01.tf
@@ -0,0 +1,37 @@
+resource "proxmox_vm_qemu" "srv_prod_01" {
+  name = "srv-prod-01"
+  desc = "Ubuntu Server"
+  vmid = "401"
+  target_node = "proxmox"
+
+  agent = 1 
+
+  clone = "ubuntu-server-focal-docker"
+  full_clone = true 
+  cores = 4
+  sockets = 1 
+  cpu = "host"
+  memory = 8192
+  
+  network {
+    bridge = "vmbr0"
+    model = "virtio"
+  }
+
+  disk {
+    storage = "local-lvm"
+    type = "virtio"
+    size = "64G"
+    format = "raw"
+  }
+  
+  scsihw = "virtio-scsi-pci"
+  os_type = "cloud-init"
+  cloudinit_cdrom_storage = "local-lvm"
+  ipconfig0 = "ip=192.168.55.3/16,gw=192.168.55.1"
+  nameserver = "192.168.55.1"
+  ciuser = "ryan"
+  sshkeys = <<EOF
+  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGugBUAze+vqUlFxob6TAFOPL0RjjnbmjIMV6n6udBZ3 rhamamur@gmail.com 
+  EOF
+}
